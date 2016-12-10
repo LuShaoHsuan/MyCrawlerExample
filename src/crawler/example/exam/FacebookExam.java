b2854b0c4d351dc65b42536ec8e927a0505c8aa0
Student <mars820502@gmail.com>
2016-12-10 16:25:56 +0800

    facebook exam fixed

--- src/crawler/example/exam/FacebookExam.java
+++ src/crawler/example/exam/FacebookExam.java
@@ -24,9 +24,9 @@ public class FacebookExam {
 		// 遠端資料路徑
 
 		String uri = 
-				"https://graph.facebook.com/v2.6"
-				+ "/search?q="
-				+ "&access_token=";
+				"https://graph.facebook.com/v2.8"
+				+ "/crazyck101/posts?fields=id,link,message,created_time,reactions.limit(0).summary(total_count)&since=1480608000&until=1481299200"
+				+ "&access_token=EAACEdEose0cBAGkGaxZBB6x1EZC9g00R8d25nGyHgmuICwV3EIIbNEJNcNvjHLOpE6SjNl35YVGxWXf2ukeXPSvfVt7nmhaZBjgUlYZBxhT0slah3pEN4Bwg1z3v7WDlzRW1AWUnZBnWXi1qObSnbI30DUw7HbYdJVhtbecqZCFAZDZD";
 
 
 		Elements elems =
@@ -34,17 +34,17 @@ public class FacebookExam {
 				.getFromJson(uri)
 				.select("data");
 		
-		String output = "id,reactions";
+		String output = "";
 
 		// 遂筆處理
 		for( Element data: elems ){
 			String id = data.select("id").text();
 
 			// FIXIT
-			String reactions = "";
+			String reactions = data.select("reactions").text();
 
 
-			output += id + "," + reactions + "\n";
+			output += "id = " + id + ", " + "reactions = " + reactions + "\n";
 		}
 
 		System.out.println( output );
